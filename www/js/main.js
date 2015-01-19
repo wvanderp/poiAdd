f0e49246744b7d03e78e37fab67415c76dd2664d
wouter van der plas <wouterv.dplas@gmail.com>
2015-01-19 20:25:35 +0100

    deleteing jquery mobile for costom css frame work and added leaflet

---
+++ www/js/main.js
@@ -0,0 +1,27 @@
+
+$( document ).ready(function(){
+	// $("#main").addClass("menu-open");
+	
+	$("#menu-button").click(function(){
+		$("#main").addClass("menu-open");
+	});
+	$("#overlay").click(function(){
+		$("#main").removeClass("menu-open");
+	});
+
+	$(".mButton li").click(function(){
+		$(".mButton li").siblings().addClass("submenu-open");
+	});
+
+
+	// create a map in the "map" div, set the view to a given place and zoom
+	var map = L.map('map', {
+	    center: [51.505, -0.09],
+	    zoom: 13
+	});
+	// add an OpenStreetMap tile layer
+	L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+	    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+	}).addTo(map);
+
+})
